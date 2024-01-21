4e1ab80d15fc53059e8e2499618eae24335a14f4
Richard M <richardmaichs276@gmail.com>
2024-01-21 03:12:51 -0500

    updated env variable and dist file

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -31,7 +31,7 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Richard",
           from_email: form.email,
           to_email: "richardmaichs276@gmail.com",
           message: form.message,
